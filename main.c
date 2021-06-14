34f9d0a24e6c0bd02631bb6ac83dbeb2aede1f2e
opurie <janpietrzak00@gmail.com>
2021-06-14 23:08:23 +0200

    sleepy

--- main.c
+++ main.c
@@ -253,7 +253,7 @@ start:
         message = receiveMessage();
         incrementTimestamp(message.timestamp);
         if(receivedACKs == countOfY-1){   
-            printf("[Y - %d] ACKSSSSSSSSS");
+            printf("[Y - %d] ACKSSSSSSSSS\n");
         }
         if(message.type == REQ){
             queue[message.sender - ys] = message.timestamp;
