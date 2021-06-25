363c20d2895d09bd534974def22126bbb8fba400
opurie <janpietrzak00@gmail.com>
2021-06-25 23:55:05 +0200

    f

--- main.c
+++ main.c
@@ -188,7 +188,7 @@ int farmingY(int k, int* queue, int* xtab){
 void runningY(){
     int *queue= malloc(countOfY * sizeof(int));
     int *xtab = malloc(countOfX * sizeof(int));
-    memset(xtab,-1, countOfX);
+    memset(xtab,0, countOfX);
     memset(queue, 0, countOfY);
     struct Message message;
 
